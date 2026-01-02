2b54d269aa6cd379bdf10b5002d0b4a9e27ae881
Hydrohaven <hydrohaven2004@gmail.com>
2026-01-02 04:34:39 -0800

    docs: added large TOOD for parseDate

--- content.js
+++ content.js
@@ -8,9 +8,8 @@ function renderCalendarButton(wrapper) {
     const startContainer = dateContainers[0];
     const endContainer = dateContainers[1];
     
-    const startDate = parseDate(startContainer, 'event-date-start');
-    const endDate = parseDate(endContainer, 'event-date-end');
-    
+    const [startDate, endDate] = parseDate();
+
     const btn = document.createElement('button');
     btn.classList.add('event-btn');
     btn.textContent = 'Add to GO Calendar';
@@ -22,9 +21,32 @@ function renderCalendarButton(wrapper) {
     });    
 }
 
-function parseDate(dateContainer, id) { // returns YYYY-MM-DD format
-    let date = new Date(document.getElementById(id).textContent)
-    return date.toISOString().split('T')[0];
+// TODO!: Mass refactor, must pass in event tag into parseDate and change functionality based on tag
+//         e.g. Raid Battles need to be All Day events and have their end date subtracted by 1
+//              Dmax Weekends need to be All Day events but have their end dates unchanged (as max battles will default end at 8/9pm)
+//              Raid Hours, Community Days, and GO Tours should have their exact hours listed as they are more time-sensitive
+//         P.S. I should also gray out the button if the calendar feature hasnt been specifically tested for a certain tag yet (e.g. GO Fest)
+//              It's just good practice to not rollout features that haven't been tested yet.
+function parseDate() { // returns YYYY-MM-DD format
+    let startDate = new Date(document.getElementById('event-date-start').textContent)
+    let endDate = new Date(document.getElementById('event-date-end').textContent)
+
+    startDate = startDate.toISOString().split('T')[0]
+    endDate = endDate.toISOString().split('T')[0]
+
+    // TODO: If raid battles tag, subtract end date by 1 and return early
+
+    let startTime = new Date(document.getElementById('event-date-start').textContent)
+    let endTime = new Date(document.getElementById('event-date-end').textContent)
+
+    
+
+    // If multi-day event, subtract end date by 1 (since 10am end times are negligible)
+    if (endDate !== startDate) {
+
+    }
+
+    return [startDate, endDate];
 }
 
 renderCalendarButton(document.getElementById('event-time-date-box'));
